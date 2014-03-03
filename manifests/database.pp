6324e40d9dff1e467aaf9e7fcfcc519f0787012e
Nuria Ruiz <nuria@wikimedia.org>
2014-03-03 13:47:24 +0100

    Changes in puppet to support a testing db for wikimetrics in vagrant.
    Created testing dbs and run alembic migration as part of puppet provisioning.
    Removed whitespace. Changed variable names. Added comments. Tested vagrant
    setup on mac and linux.
    
    Change-Id: I8f8d4df4eb63e5f035dae443faa4dae10e276370

--- manifests/database.pp
+++ manifests/database.pp
@@ -7,18 +7,26 @@
 # for remote hosts to connect to MySQL and the wikimetrics database manually.
 #
 # == Parameters
-# $db_user        - username of wikimetrics database user.  Default: wikimetrics
-# $db_pass        - password of wikimetrics database user.  Default: wikimetrics
-# $db_name        - name of wikimetrics database.           Default: wikimetrics
-# $db_root_user   - Name of user MySQL commands will be executed as.  Default: 'root'
-# $db_root_pass   - Password for $db_root_user.  Default: no password
+# $db_user                       - username of wikimetrics database user.    Default: wikimetrics
+# $db_pass                       - password of wikimetrics database user.    Default: wikimetrics
+# $db_name_wikimetrics           - name of wikimetrics database.    Default: wikimetrics
+# $db_name_wikimetrics_testing   - name of wikimetrics database used in testing.    Default: wikimetrics
+# $db_name_mediawiki_testing     - name of wiki database used in tests. Default: wiki_testing
+# $db_root_user                  - Name of user MySQL commands will be executed as.  Default: 'root'
+# $db_root_pass                  - Password for $db_root_user.  Default: no password
+# $wikimetrics_path              - Path to wikimetrics source.  Default:: '/srv/wikimetrics'
 #
 class wikimetrics::database(
-    $db_user      = 'wikimetrics',
-    $db_pass      = 'wikimetrics', # you should really change this one
-    $db_name      = 'wikimetrics',
-    $db_root_user = 'root',
-    $db_root_pass = undef,
+    $db_user                     = 'wikimetrics',
+    $db_pass                     = 'wikimetrics', # you should really change this one
+    $db_name_wikimetrics         = 'wikimetrics',
+    $db_name_wikimetrics_testing = 'wikimetrics_testing',
+    $db_name_mediawiki_testing   = 'wiki_testing',
+    $db_root_user                = 'root',
+    $db_root_pass                = undef,
+    $wikimetrics_path            = '/srv/wikimetrics',
+
+
 )
 {
     # mysql-server must be installed on this node to use this class.
@@ -35,20 +43,55 @@ class wikimetrics::database(
         default => "-p'${db_root_pass}'",
     }
 
-    # wikimetrics is going to need a wikimetrics database and user.
-    exec { 'wikimetrics_mysql_create_database':
-        command => "/usr/bin/mysql ${username_option} ${password_option} -e \"CREATE DATABASE ${db_name}; USE ${db_name};\"",
-        unless  => "/usr/bin/mysql ${username_option} ${password_option} -e 'SHOW DATABASES' | /bin/grep -q ${db_name}",
+    # create wikimetrics_testing
+    exec { 'wikimetrics_testing_mysql_create_database':
+        command => "/usr/bin/mysql ${username_option} ${password_option} -e \"CREATE DATABASE ${db_name_wikimetrics_testing}; USE ${db_name_wikimetrics_testing};\"",
+        unless  => "/usr/bin/mysql ${username_option} ${password_option} -e 'SHOW DATABASES' | /bin/grep -q ${db_name_wikimetrics_testing}",
         user    => 'root',
     }
+
+    # wikimetrics is going to need a wikimetrics database and user.
+    exec { 'wikimetrics_mysql_create_database':
+        command => "/usr/bin/mysql ${username_option} ${password_option} -e \"CREATE DATABASE ${db_name_wikimetrics}; USE ${db_name_wikimetrics};\"",
+        unless  => "/usr/bin/mysql ${username_option} ${password_option} -e 'SHOW DATABASES' | /bin/grep -v ${db_name_wikimetrics_testing} | /bin/grep -q ${db_name_wikimetrics}",
+        user    => 'root',
+    }
+
+    # create wiki testing (is there a way not to repeat this like ahem three times?)
+    exec { 'wiki_testing_mysql_create_database':
+        command => "/usr/bin/mysql ${username_option} ${password_option} -e \"CREATE DATABASE ${db_name_mediawiki_testing}; USE ${db_name_mediawiki_testing};\"",
+        unless  => "/usr/bin/mysql ${username_option} ${password_option} -e 'SHOW DATABASES' | /bin/grep -q ${db_name_mediawiki_testing}",
+        user    => 'root',
+    }
+
     exec { 'wikimetrics_mysql_create_user':
         command => "/usr/bin/mysql ${username_option} ${password_option} -e \"
 CREATE USER '${db_user}'@'localhost' IDENTIFIED BY '${db_pass}';
 CREATE USER '${db_user}'@'127.0.0.1' IDENTIFIED BY '${db_pass}';
-GRANT ALL PRIVILEGES ON ${db_name}.* TO '${db_user}'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON ${db_name}.* TO '${db_user}'@'127.0.0.1' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${db_name_wikimetrics}.*         TO '${db_user}'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${db_name_wikimetrics}.*         TO '${db_user}'@'127.0.0.1' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${db_name_wikimetrics_testing}.* TO '${db_user}'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${db_name_wikimetrics_testing}.* TO '${db_user}'@'127.0.0.1' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${db_name_mediawiki_testing}.*        TO '${db_user}'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${db_name_mediawiki_testing}.*        TO '${db_user}'@'127.0.0.1' WITH GRANT OPTION;
 FLUSH PRIVILEGES;\"",
         unless  => "/usr/bin/mysql ${username_option} ${password_option} -e \"SHOW GRANTS FOR '${db_user}'@'127.0.0.1'\" | grep -q \"TO '${db_user}'\"",
         user    => 'root',
+        require => [Exec['wiki_testing_mysql_create_database'],Exec['wikimetrics_mysql_create_database'],Exec['wikimetrics_testing_mysql_create_database']],
     }
+
+    # In wikimetrics.pp we are installing all deps wia pip
+    # should be safe to assume alembic is installed
+    # this would run only if alembic is not setup
+    # as we prefer puppet not handle migrations
+    exec { 'alembic_upgrade_head':
+        cwd =>  $wikimetrics_path,
+        command => "/usr/local/bin/alembic upgrade head",
+        unless  => "/usr/bin/mysql ${username_option} ${password_option} -e \"USE ${db_name_wikimetrics};SHOW tables\"| grep alembic ",
+        user    => 'root',
+        require => Exec['wikimetrics_mysql_create_user'],
+    
+     }
+
+    
 }
